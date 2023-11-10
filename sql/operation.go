a21f3e02f1fd1011d8c0314eb3e1bda8c4a00fc9
snilli <mklx1319@gmail.com>
2023-11-10 17:45:56 +0700

    update ops in to bind params

--- sql/operation.go
+++ sql/operation.go
@@ -2,9 +2,15 @@ package operation
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func In(ref []string) string {
-	return fmt.Sprintf("(%s)", strings.Join(ref, ","))
+	var params = []string{}
+	for index := range ref {
+		params = append(params, fmt.Sprintf("$%s", strconv.Itoa(index+1)))
+	}
+
+	return fmt.Sprintf("(%s)", strings.Join(params, ","))
 }
