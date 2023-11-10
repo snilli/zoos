46af6a282a24b8c964e3de738ce020a3643e5df5
snilli <mklx1319@gmail.com>
2023-11-10 14:28:28 +0700

    backward inline func

--- sql/operation.go
+++ sql/operation.go
@@ -1,20 +1,10 @@
-package sql
+package operation
 
 import (
 	"fmt"
 	"strings"
 )
 
-type sqlOperations struct{}
-
-// type sqlOperations interface {
-// 	In(ref []string) string
-// }
-
-func NewSqlOperation() sqlOperations {
-	return sqlOperations{}
-}
-
-func (op *sqlOperations) In(ref []string) string {
+func In(ref []string) string {
 	return fmt.Sprintf("(%s)", strings.Join(ref, ","))
 }
