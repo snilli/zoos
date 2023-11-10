083c6eba70e1093af1b9f2c15016a061bd833f3a
snilli <mklx1319@gmail.com>
2023-11-10 17:48:50 +0700

    binding sql befor in

--- sql/operation.go
+++ sql/operation.go
@@ -6,11 +6,11 @@ import (
 	"strings"
 )
 
-func In(ref []string) string {
+func In(sql string, ref []string) string {
 	var params = []string{}
 	for index := range ref {
 		params = append(params, fmt.Sprintf("$%s", strconv.Itoa(index+1)))
 	}
 
-	return fmt.Sprintf("(%s)", strings.Join(params, ","))
+	return fmt.Sprintf("%s in (%s)", sql, strings.Join(params, ","))
 }
